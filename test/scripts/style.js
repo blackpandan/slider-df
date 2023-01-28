daee167399cb03343fd3fdd6837832e9d0816af8
blackpandan <faithgstc54@gmail.com>
2023-01-28 13:16:50 +0100

    New: setup folders for package and test

---
+++ test/scripts/style.js
@@ -0,0 +1,5 @@
+let scrollbarWidth = document.body.offsetHeight - document.body.clientHeight;
+
+let main = document.getElementById("app");
+
+// main.style.cssText = `--scrollwidth: ${scrollbarWidth / 10}px`;
